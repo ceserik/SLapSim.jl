9af3b4c1884242407d2c338dbb3c5fb9ba44c8a7
Richard <ciglanskyri@gmail.com>
2025-09-20 15:18:12 +0200

    rewritten optimal trajectory as function

--- src/optInterface.jl
+++ src/optInterface.jl
@@ -45,49 +45,56 @@ track = 0
 #track = singleTurn()
 path = "tracks/FSCZ.kml"
 track = kml2track(path,true)
-N = length(track.curvature)# pridat parameter trate ze kolko ma indexov a aka je dlha v metroch
-
-#fill track parameters which are constant along track, should be automatized
-track.rho    = fill(track.rho,N)
-track.μ      = fill(track.μ,N)
-track.widthL = fill(1.5,N)
-track.widthR = fill(1.5,N)
-
-model = JuMP.Model(Ipopt.Optimizer) 
-#create inputs for car model #create instance of track parameters
-
-#determine sizes of inputs and states
-nControls = Int(round(car.carParameters.nControls.value))
-@variable(model,U[1:N-1,1:nControls],start = 0.0)
-@variable(model,X[1:N,1:6], start = 5.0)
-
-N = length(track.sampleDistances)
-s = track.sampleDistances#1:length(track.curvature)
-t_final = X[end,6]
+model = JuMP.Model(Ipopt.Optimizer)
 
 
-for k = 1:N-1 # loop over control intervals
-    x_next = X[k,:] + (s[k+1]-s[k]) * carF(k, U[k,:], X[k,:]);
-    @constraint(model,X[k+1,:] .== x_next)
+function findOptimalTrajectory(track,car,model)
+    N = length(track.sampleDistances)
+
+    #fill track parameters which are constant along track, should be automatized
+    track.rho    = fill(track.rho,N)
+    track.μ      = fill(track.μ,N)
+    track.widthL = fill(track.widthL,N)
+    track.widthR = fill(track.widthR,N)
+
+     
+    #create inputs for car model #create instance of track parameters
+
+    #determine sizes of inputs and states
+    nControls = Int(round(car.carParameters.nControls.value))
+    @variable(model,U[1:N-1,1:nControls],start = 0.0)
+    @variable(model,X[1:N,1:6], start = 5.0)
+
+    
+    s = track.sampleDistances#1:length(track.curvature)
+    t_final = X[end,6]
+
+
+    for k = 1:N-1 # loop over control intervals
+        x_next = X[k,:] + (s[k+1]-s[k]) * carF(k, U[k,:], X[k,:]);
+        @constraint(model,X[k+1,:] .== x_next)
+    end
+
+    #initial states
+    #        [vx vy psi dpsi n t]
+    #initial = [4; 0; 0; 0; 0; 0]
+    #set initial states
+    #@constraint(model,X[1,:] .== initial)
+
+    @constraint(model,X[1:end,1] .>= 0)
+    @constraint(model,X[1,1] .== 5)
+    @constraint(model,X[1,6] .>= 0)
+    @constraint(model,diff(X[:,6]) .>=0)
+
+
+    @objective(model,Min,X[end,6])
+    optimize!(model)
+
+    # Plotting the results
+    fig = Figure(resolution = (800, 600))
+    ax = Axis(fig[1, 1], xlabel = "Step", ylabel = "Velocity (vx)")
+    lines!(ax, value.(X[:,1]))
+    display(GLMakie.Screen(), fig)  # This creates a new window
 end
 
-#initial states
-#        [vx vy psi dpsi n t]
-#initial = [4; 0; 0; 0; 0; 0]
-#set initial states
-#@constraint(model,X[1,:] .== initial)
-
-@constraint(model,X[1:end,1] .>= 0)
-@constraint(model,X[1,1] .== 5)
-@constraint(model,X[1,6] .>= 0)
-@constraint(model,diff(X[:,6]) .>=0)
-
-
-@objective(model,Min,X[end,6])
-optimize!(model)
-
-# Plotting the results
-fig = Figure(resolution = (800, 600))
-ax = Axis(fig[1, 1], xlabel = "Step", ylabel = "Velocity (vx)")
-lines!(ax, value.(X[:,1]))
-display(GLMakie.Screen(), fig)  # This creates a new window
+findOptimalTrajectory(track,car,model)
