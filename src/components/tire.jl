28df920825906ce623f886ba40731aa1f6af0421
Richard <ciglanskyri@gmail.com>
2026-04-06 14:23:13 +0200

    added regression tire

--- src/components/tire.jl
+++ src/components/tire.jl
@@ -231,5 +231,90 @@ function createR20_pacejka(motor::Motor,gearbox::Gearbox)
     )
 end
 
+function create_FormulaE_pacejka(motor::Motor,gearbox::Gearbox)
+    radius = carParameter{carVar}(0.33, "tire radius", "m")
+    width = carParameter{carVar}(0.3, "tire width", "m")
+    inertia = carParameter{carVar}(0.0, "tire inerti", "m")
+    mass = carParameter{carVar}(0.0, "tire mass,wrong", "kg")
+    velocity = carParameter{Vector{carVar}}([0.0, 0.0, 0.0], "velocity", "m/s")
+    angularFrequency = carParameter{carVar}(0.0, "angular velocity", "rad/s")
+    forces = carParameter{Vector{carVar}}([0.0, 0.0, 0.0], "Tire Force", "N")
+    slipAngle = carParameter{carVar}(0.0, "Slip angle", "rad")
+    slipRatio = carParameter{carVar}(0.0, "slip ratio", "-")
+    maxSlipAngle = carParameter{carVar}(5/180*pi, "max slip angle", "rad")
+    scalingForce = carParameter{carVar}(0.0, "max longitudinal force", "N")
+    scalingForce.value = motor.torqueSpeedFunction(0.0) * gearbox.ratio.value / radius.value
+    frictionCoefficient = carParameter{carVar}(1.0, "Friction Coefficient", "-", :tunable)
+    rollingResistance = carParameter{carVar}(-0.010, "Rolling resistance coeff", "-", :tunable)
+    brakingForce = carParameter{carVar}(0.0, "braking force", "N")
+    # Pacejka Magic Formula coefficients
+    B = 9.62   # stiffness factor
+    C = 2.59  # shape factor
+    E = 1  # curvature factor
 
+    function setVelocity(velocityIn::Vector{carVar})
+        velocity.value = velocityIn
+    end
+
+    function compute(inTorque::carVar, optiModel::Union{JuMP.Model,Nothing}=nothing)
+        slipAngle.value = -atan(velocity.value[2], velocity.value[1])
+        α = slipAngle.value * 1
+        D = forces.value[3] * frictionCoefficient.value(1 + -0.0813 * forces.value[3]/3000)
+        forces.value[2] = D * sin(C * atan(B * α - E * (B * α - atan(B * α))))
+        forces.value[1] = inTorque/radius.value + brakingForce.value + forces.value[3] * rollingResistance.value
+    end
+
+    function tireConstraints(model=nothing)
+        μFz = frictionCoefficient.value * forces.value[3]
+        fx = forces.value[1] / μFz
+        fy = forces.value[2] / μFz
+        lessContraint(fx^2 + fy^2, 1.0, model)
+    end
+
+    function setupObservables(ax::Axis)
+        dummy = _rect_points(0.0, 0.0, 1.0, 1.0, 0.0)
+        rect_obs = Observable(dummy)
+        poly!(ax, rect_obs; color=(:black, 0.3), strokecolor=(:gray30, 0.5), strokewidth=1)
+        ellipse_obs = Observable([Point2f(0, 0) for _ in _ELLIPSE_ANGLES])
+        lines!(ax, ellipse_obs; color=(:gray, 0.5), linewidth=1, linestyle=:dash)
+        force_pos_obs = Observable([Point2f(0, 0)])
+        force_dir_obs = Observable([Point2f(0, 0)])
+        arrows2d!(ax, force_pos_obs, force_dir_obs; color=:green, shaftwidth=4, tipwidth=8, tiplength=8)
+        return (rect=rect_obs, ellipse=ellipse_obs, force_pos=force_pos_obs, force_dir=force_dir_obs)
+    end
+
+    function updateObservables(obs::NamedTuple, wx::Float64, wy::Float64, θ::Float64, Fz_static::Float64)
+        r = radius.value
+        s = 4r / Fz_static
+        R = _rotmat2d(θ)
+        obs.rect[] = _rect_points(wx, wy, 2r, width.value * 0.8, θ)
+        Fmax_s = abs(forces.value[3]) * frictionCoefficient.value * s
+        obs.ellipse[] = [Point2f(R * [Fmax_s * cos(a), Fmax_s * sin(a)] + [wx, wy]) for a in _ELLIPSE_ANGLES]
+        f_global = R * [forces.value[1], forces.value[2]] .* s
+        obs.force_pos[] = [Point2f(wx, wy)]
+        obs.force_dir[] = [Point2f(f_global[1], f_global[2])]
+    end
+
+    tire = Tire(
+        radius,
+        width,
+        inertia,
+        mass,
+        velocity,
+        angularFrequency,
+        forces,
+        slipAngle,
+        slipRatio,
+        compute,
+        tireConstraints,
+        setVelocity,
+        maxSlipAngle,
+        scalingForce,
+        frictionCoefficient,
+        rollingResistance,
+        brakingForce,
+        setupObservables,
+        updateObservables,
+    )
+end
 
